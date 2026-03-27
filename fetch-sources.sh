09765577f12b28c3ccd6004ff68b1378affa74b0
Condor <condor@stz-bg.com>
2026-03-27 10:22:00 +0200

    Fix broken submodule state after clean/deinit

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -12,13 +12,25 @@ mkdir -p $CWD/sources
 
 # fetching main sources
 if [ -d $ROCM_LIBRARIES_DIR ]; then
-    ( cd $ROCM_LIBRARIES_DIR && git clean -f -d && git reset --hard HEAD && git fetch --all --tags && git submodule deinit -f --all && git checkout ${ROCM_BRANCH} )
+    ( cd $ROCM_LIBRARIES_DIR && 
+    git fetch --all --tags &&
+    git reset --hard HEAD &&
+    git clean -ffdx &&
+    git checkout ${ROCM_BRANCH} &&
+    git submodule sync --recursive &&
+    git submodule update --init --recursive )
 else
     ( cd $ROCM_REL_DIR && git clone -j 4 --branch ${ROCM_BRANCH} https://github.com/ROCm/rocm-libraries.git )
 fi
 
 if [ -d $ROCM_SYSTEMS_DIR ]; then
-    ( cd $ROCM_SYSTEMS_DIR && git clean -f -d && git reset --hard HEAD && git fetch --all --tags && git submodule deinit -f --all && git checkout ${ROCM_BRANCH} )
+    ( cd $ROCM_SYSTEMS_DIR && 
+    git fetch --all --tags &&
+    git reset --hard HEAD &&
+    git clean -ffdx &&
+    git checkout ${ROCM_BRANCH} &&
+    git submodule sync --recursive &&
+    git submodule update --init --recursive )
 else
     ( cd $ROCM_REL_DIR && git clone -j 4 --branch ${ROCM_BRANCH} https://github.com/ROCm/rocm-systems.git )
 fi
